b2c2ac0a8c7cae0e2b3f063e82a181647a746563
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2024-02-26 23:41:59 +0600

    tracer name change

--- src/span.ts
+++ src/span.ts
@@ -1,7 +1,7 @@
 import { api } from '@opentelemetry/sdk-node';
 import { setupTracing } from './tracer';
 
-export const tracer = setupTracing('test-service');
+export const tracer = setupTracing('book-catalog-service');
 
 export const span = tracer.startSpan('book-creation', {
    kind: api.SpanKind.CLIENT,
